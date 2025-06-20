f3d8069d17b79a9163b076257ac385d4ff798b6e
TheQueCode <thequecode@gmail.com>
2025-06-20 08:08:28 -0500

    Buttons, eventlisteners, style

--- js/script.js
+++ js/script.js
@@ -4,18 +4,24 @@ const calculator = document.createElement('div');
 calculator.className = 'calculator';
 
 const display = document.createElement('input');
+display.id = 'display';
 display.className = 'display';
 
-const button = document.createElement('button');
-button.className = 'button';
+const buttonContainer = document.createElement('div');
+buttonContainer.className = 'buttonContainer';
 
 container.appendChild(calculator);
 calculator.appendChild(display);
-calculator.appendChild(button);
+calculator.appendChild(buttonContainer);
 
-
-
-
+for (let i = 0; i < 12; i++){
+  const button = document.createElement('button');
+  button.innerHTML = `${i + 1}`;
+  button.className = `button`;
+  button.id = `button${i + 1}`;
+  button.addEventListener('click', console.log(button.innerText));
+  buttonContainer.appendChild(button);
+}
 
 
 function operate (operator, num1, num2) {
